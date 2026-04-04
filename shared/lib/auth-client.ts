e7eef3f74b3317fa85b0a1e6b3043bd1ff6e8179
ivan2214 <bongiovanniivan12@gmail.com>
2026-04-04 16:59:03 -0300

    add custom session

--- shared/lib/auth-client.ts
+++ shared/lib/auth-client.ts
@@ -1,9 +1,19 @@
 "use client";
 
+import {
+  customSessionClient,
+  inferAdditionalFields,
+} from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+
+import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+  plugins: [
+    customSessionClient<typeof auth>(),
+    inferAdditionalFields<typeof auth>(),
+  ],
 });
 
 export const { signIn, signUp, signOut, useSession, getSession } = authClient;
